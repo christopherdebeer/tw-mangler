9188711b8a35ebdac092e1b921f29c20dcc39def
Christopher de Beer <christopherdebeer@gmail.com>
2011-11-25 14:53:39 +0000

    ahem removed keys

--- bot.js
+++ bot.js
@@ -23,13 +23,13 @@ var doneTweets = [];
 var oauthConsumer = new OAuth(
     "http://twitter.com/oauth/request_token",
     "http://twitter.com/oauth/access_token", 
-    "dpWq5IKoWJTHv3RRhL9Lrg", "vUk3pa2ZUW1h4gDewvNsu8uKVIKC1Tj51iPOs5Gc", 
+    "xxxx", "xxxxxx", 
     "1.0", null, "HMAC-SHA1");
 
 
 var twitterClient = tweasy.init(oauthConsumer, {
-  access_token : "420351901-R3fxM6UNkJe4HbETFIzuLgkm7tfNUSHFU1BZhpM",
-  access_token_secret : "aUsYRqdIidhRDGtkizbLIQyCWRsfSVYHZ9jTVAWsow"
+  access_token : "xxxxx",
+  access_token_secret : "xxxxxx"
 });
 
 
